e529ccf685cef664c40d15ca0fd7f6f1926e7438
Aditya Khandelwal <adityakhandelwal4201@gmail.com>
2021-07-31 04:03:51 +0530

    Experience added

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -5,12 +5,16 @@ function Experience() {
     return (
         <div className="experienceContainer">
             <div className="experience">
-                Experience
+                Internships
             </div>
             <div className="experienceDetails">
-                May 2021 - Present <br/>
-                Associate Data Engineer (Intern)<br/>
-                The Tann Mann Gaadi <br/>
+                <span id="intern">May 2021 - July 2021 <br/>
+                Associate Data Engineer (Intern) at The Tann Mann Gaadi <br/></span>
+                &bull;<span className ="detail">Developed graphs in react using the recharts library for dashboard.</span> <br/>
+                &bull;<span className ="detail">Developed authentication and integrated the pages with the rest APIs using AXIOS </span> <br/>
+                <span className="temp-details">(APIs were developed in Node JS).</span>
+                <br/>&bull;<span className ="detail">Developed few APIs in Nodejs.</span>
+                <br/>&bull;<span className ="detail">Led the frontend development of the project.<br/></span>
             </div>
         </div>
     )
--- src/components/ProfileImage.js
+++ src/components/ProfileImage.js
@@ -1,32 +1,44 @@
 import React from "react";
-import { Facebook, LinkedIn, Instagram, Twitter } from "@material-ui/icons";
+import {Facebook, LinkedIn, Instagram, Twitter} from "@material-ui/icons";
+import { makeStyles } from '@material-ui/core/styles';
 import profileImage from "../image/New.jpg";
 import "./ProfileImage.css";
 
+const useStyles = makeStyles({
+  root: {
+    padding: '0 10px',
+    marginTop:'10px'
+  },
+});
+
 function ProfileImage() {
-  return (
-    <div className="intro">
-      <img className="profileImage" src={profileImage} alt="User Profile" />
-      <div className="name-intro">
-        <h1>ADITYA KHANDELWAL</h1>
-        <h2>Final Year Student at NIT Jalandhar</h2>
-        <p className="aboutMe">
-          Graduating in 2022. Currently, leading a team of react engineers in
-          the Tann Mann Foundation Project and leading two projects together.
-          One project is for a social cause and another one is the commercial
-          one. Still learning many things, but most importantly in my time at
-          The Tann Mann Foundation, gained a lot of experience in all the
-          fields, plus in react too.
-        </p>
-        <div className="icons">
-            <Facebook fontSize="large" className="paddding-icons" />
-            <Twitter fontSize="large" className="paddding-icons"/>
-            <Instagram fontSize="large" className="paddding-icons"/>
-            <LinkedIn fontSize="large" className="paddding-icons"/>
+  const classes=useStyles();
+    return (
+        <div className="intro">
+            <img className="profileImage"
+                src={profileImage}
+                alt="User Profile"/>
+            <div className="name-intro">
+                <h1>ADITYA KHANDELWAL</h1>
+                <h2>Final Year Student at NIT Jalandhar</h2>
+                <p className="aboutMe">
+                    Graduating in 2022. Currently, leading a team of react engineers in
+                    the Tann Mann Foundation Project and leading two projects together.
+                    One project is for a social cause and another one is the commercial
+                    one. Still learning many things, but most importantly in my time at
+                    The Tann Mann Foundation, gained a lot of experience in all the
+                    fields, plus in react too.
+                </p>
+                <div className="icons">
+                    <a href="https://www.facebook.com/aditya4201/" rel="noreferrer" className="activeIcon" target="_blank" id="facebook"><Facebook fontSize="large"
+                            className={classes.root}/></a>
+                    <a href="https://twitter.com/Aditya_K4" rel="noreferrer" className="activeIcon" target="_blank" id="twitter"><Twitter fontSize="large" className={classes.root}/></a>
+                    <a href="https://www.instagram.com/adityak4201/" rel="noreferrer" className="activeIcon" target="_blank" id="instagram"><Instagram fontSize="large" className={classes.root}/></a>
+                    <a href="https://www.linkedin.com/in/adityak4201/" rel="noreferrer" className="activeIcon" target="_blank" id="linkedin"><LinkedIn fontSize="large" className={classes.root}/></a>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default ProfileImage;
